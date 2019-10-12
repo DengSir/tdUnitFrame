745f0115ae424a9ea63377872e92f389b36954e7
Dencer <tdaddon@163.com>
2019-10-12 09:41:25 +0800

    update

--- Addon.lua
+++ Addon.lua
@@ -3,8 +3,7 @@ Addon.lua
 @Author  : DengSir (tdaddon@163.com)
 @Link    : https://dengsir.github.io
 ]]
-
-local function alpha(ui, alpha)
+ local function alpha(ui, alpha)
     if not ui then
         return
     end
@@ -13,7 +12,7 @@ end
 
 local function initFrameFonts(frame)
     if frame.deadText then
-        frame.deadText:SetFont(STANDARD_TEXT_FONT, 13)--, 'OUTLINE')
+        frame.deadText:SetFont(STANDARD_TEXT_FONT, 13) -- , 'OUTLINE')
         frame.deadText:SetTextColor(RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b)
         frame.deadText:SetAlpha(0.6)
     end
@@ -52,6 +51,8 @@ for i, frame in ipairs({TargetFrame, FocusFrame}) do
     frame.nameBackground:Hide()
     frame.nameBackground.Show = nop
 
+    -- frame.name:SetPoint('CENTER', -50, 36)
+
     frame.healthbar.lockColor = true
     frame.healthbar:ClearAllPoints()
     frame.healthbar:SetPoint('BOTTOMRIGHT', frame, 'TOPRIGHT', -106, -53)
@@ -71,7 +72,12 @@ initFrameFonts(PartyMemberFrame3)
 initFrameFonts(PartyMemberFrame4)
 
 hooksecurefunc('TargetFrame_CheckClassification', function(self)
-    if UnitClassification(self.unit) == 'minus' then
+    local classification = UnitClassification(self.unit)
+    if classification == 'rareelite' then
+        self.borderTexture:SetTexture([[Interface\TargetingFrame\UI-TargetingFrame-Rare-Elite]])
+    end
+
+    if classification == 'minus' then
         self.healthbar:SetHeight(12)
         -- self.name:SetPoint('CENTER', -50, 19)
     else
@@ -81,21 +87,9 @@ hooksecurefunc('TargetFrame_CheckClassification', function(self)
     end
 end)
 
-local function UnpackColor(color)
-    return color.r, color.g, color.b
-end
-
 local function UnitClassColor(unit)
-    return UnpackColor(RAID_CLASS_COLORS[select(2, UnitClass(unit))])
+    return RAID_CLASS_COLORS[select(2, UnitClass(unit))]:GetRGB()
 end
-
--- local function UnitSelectionColor(unit)
---     local reaction = UnitReaction(unit, 'player')
---     if reaction then
---         return UnpackColor(FACTION_BAR_COLORS[reaction])
---     end
---     return _G.UnitSelectionColor(unit)
--- end
 
 hooksecurefunc('TargetFrame_CheckFaction', function(self)
     -- if UnitIsPlayer(self.unit) then
@@ -105,7 +99,7 @@ hooksecurefunc('TargetFrame_CheckFaction', function(self)
     --     self.name:SetTextColor(NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b)
     -- end
 
-    if UnitIsPlayer(self.unit) and UnitClass(self.unit) then
+    if UnitIsPlayer(self.unit) then
         self.healthbar:SetStatusBarColor(UnitClassColor(self.unit))
         -- self.name:SetTextColor(UnitSelectionColor(self.unit))
     else
@@ -116,6 +110,18 @@ hooksecurefunc('TargetFrame_CheckFaction', function(self)
         end
         -- self.name:SetTextColor(NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b)
     end
+
+    -- if self.showPVP and not self.pvpIcon:IsShown() then
+    --     local factionGroup = UnitFactionGroup(self.unit)
+
+    --     self.pvpIcon:SetTexture('Interface\\TargetingFrame\\UI-PVP-' .. factionGroup)
+    --     self.pvpIcon:Show()
+    --     self.pvpIcon:SetDesaturated(true)
+    --     self.pvpIcon:SetAlpha(0.7)
+    -- else
+    --     self.pvpIcon:SetDesaturated(false)
+    --     self.pvpIcon:SetAlpha(1)
+    -- end
 end)
 
 hooksecurefunc('PlayerFrame_ToPlayerArt', function()
@@ -138,3 +144,21 @@ function HealthBar_OnValueChanged(bar, ...)
     end
     return orig_HealthBar_OnValueChanged(bar, ...)
 end
+
+hooksecurefunc('PlayerFrame_UpdateLevelTextAnchor', function()
+    PlayerLevelText:SetPoint('CENTER', PlayerFrameTexture, 'CENTER', -63, -16)
+end)
+
+-- function PlayerFrame_UpdateLevelTextAnchor(level)
+--     PlayerLevelText:SetPoint('CENTER', PlayerFrameTexture, 'CENTER', -63, -17)
+-- end
+
+-- PlayerLevelText:SetPoint('CENTER', PlayerFrameTexture, 'CENTER', -63, -16)
+-- PlayerLevelText.SetPoint = nop
+
+-- TargetFrame.levelText:SetPoint('CENTER', 63, -16)
+-- TargetFrame.levelText.SetPoint = nop
+
+hooksecurefunc('TargetFrame_UpdateLevelTextAnchor', function(self)
+    self.levelText:SetPoint('CENTER', 63, -16)
+end)
